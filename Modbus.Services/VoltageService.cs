ea68e340b8da68b22dc8e8870ca59d9bbcaf9b8c
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-05-09 08:18:09 +0200

    Dodanie usługi do sterowania wyjściem

--- Modbus.Services/VoltageService.cs
+++ Modbus.Services/VoltageService.cs
@@ -1,21 +1,55 @@
-﻿using System;
+﻿using Modbus.Device;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Modbus.Services
 {
-    public class VoltageService : IAnalogOutputService
+    public class VoltageService : BaseService, IAnalogOutputService
     {
-        public void Set(float value)
+        public VoltageService(string hostname, int port, byte slaveId)
+            : base(hostname, port, slaveId)
         {
-            throw new NotImplementedException();
         }
 
-        public Task SetAsync(float value)
+        public void Set(float value)
         {
-            throw new NotImplementedException();
+            Trace.WriteLine($"Connecting to {hostname}:{port}");
+
+            ushort startAddress = 4100;
+
+            using (var client = new TcpClient(hostname, port))
+            {
+                ModbusIpMaster master = ModbusIpMaster.CreateIp(client);
+
+                var result = Converter.ConvertToUshort(value);
+
+                ushort[] values = new ushort[2] { result[0], result[1] };
+
+                master.WriteMultipleRegisters(slaveId, startAddress, values);
+            }
+        }
+
+        public async Task SetAsync(float value)
+        {
+            Trace.WriteLine($"Connecting to {hostname}:{port}");
+
+            ushort startAddress = 4100;
+
+            using (var client = new TcpClient(hostname, port))
+            {
+                ModbusIpMaster master = ModbusIpMaster.CreateIp(client);
+
+                var result = Converter.ConvertToUshort(value);
+
+                ushort[] values = new ushort[2] { result[0], result[1] };
+
+                await master.WriteMultipleRegistersAsync(slaveId, startAddress, values);
+            }
         }
     }
 }
